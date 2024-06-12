4b13e38b0547c6137f60a4a16dfa3cc0254d012a
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-06-12 06:47:51 -0600

    Update aws_s3.tf

--- infra/aws_s3.tf
+++ infra/aws_s3.tf
@@ -6,26 +6,6 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
-resource "aws_s3_bucket" "mgt" {
-  bucket        = "${var.deployment_name}-${random_string.unique_id.result}-mgt"
-  force_destroy = true
-  tags = merge({
-    Name = "${var.deployment_name}-${random_string.unique_id.result}-mgt"
-  })
-}
-
-
-resource "aws_s3_bucket_versioning" "mgt" {
-  bucket = aws_s3_bucket.mgt.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
-
-
 output "s3_bucket" {
   value = aws_s3_bucket.data.bucket
 }
