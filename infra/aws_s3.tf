e737bbdc7e9902ab62268b6d98282167db376235
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-06-12 07:43:31 -0600

    Update infra/aws_s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- infra/aws_s3.tf
+++ infra/aws_s3.tf
@@ -14,6 +14,19 @@ resource "aws_s3_bucket" "mgt" {
   })
 }
 
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "mgt" {
+  bucket = aws_s3_bucket.mgt.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+
+
 output "s3_bucket" {
   value = aws_s3_bucket.data.bucket
 }
