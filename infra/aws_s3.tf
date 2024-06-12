dd95d187aa95508cccdba18949ae147aa7146b25
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-06-12 06:40:53 -0600

    Update aws_s3.tf

--- infra/aws_s3.tf
+++ infra/aws_s3.tf
@@ -6,6 +6,14 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
+resource "aws_s3_bucket" "mgt" {
+  bucket        = "${var.deployment_name}-${random_string.unique_id.result}-mgt"
+  force_destroy = true
+  tags = merge({
+    Name = "${var.deployment_name}-${random_string.unique_id.result}-mgt"
+  })
+}
+
 
 output "s3_bucket" {
   value = aws_s3_bucket.data.bucket
