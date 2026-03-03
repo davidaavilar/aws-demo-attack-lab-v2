5ee36002bd369f8f085013b56e85fcf5086151d0
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-03-03 09:23:57 -0500

    updated versions

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.6.0"
+  version = "~> 6.0"
 
   name = "${var.deployment_name}-${random_string.unique_id.result}"
 
