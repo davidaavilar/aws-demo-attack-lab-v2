c6aaf4e1c7dd1ba523eeb374262b16ae111cdb29
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-03-10 08:20:43 -0700

    Update aws_eks-cluster.tf

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0"
 
   name                   = "${var.deployment_name}-${random_string.unique_id.result}"
-  kubernetes_version     = "1.29"
+  kubernetes_version     = "1.30"
   vpc_id                 = module.vpc.vpc_id
   subnet_ids             = module.vpc.private_subnets
   endpoint_public_access = true
@@ -11,7 +11,7 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name           = "${var.deployment_name}-ng1"
-      instance_types = ["t3.large"]
+      instance_types = ["t4g.large"]
 
       ami_type = "AL2023_x86_64_STANDARD"
 
