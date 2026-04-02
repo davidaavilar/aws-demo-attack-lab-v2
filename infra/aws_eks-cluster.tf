09f26629890bfd1e27d6a50cb62081a4e6802229
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-04-02 18:09:24 -0500

    deleted

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -26,3 +26,29 @@ module "eks" {
     }
   }
 }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 21.0"
+
+#   name               = "${var.deployment_name}-${random_string.unique_id.result}"
+#   kubernetes_version = "1.30"
+
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+
+#   endpoint_public_access = true
+
+#   eks_managed_node_groups = {
+#     default = {
+#       instance_types = ["t3.medium"]
+#       min_size       = 1
+#       max_size       = 2
+#       desired_size   = 1
+
+#       ami_type = "AL2023_x86_64_STANDARD"
+
+#       capacity_type = "ON_DEMAND"
+#     }
+#   }
+# }
