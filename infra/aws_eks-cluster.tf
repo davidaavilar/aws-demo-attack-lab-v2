7800c8ca1e3a0429214de880e48afc2d8a4d66b9
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-04-02 18:39:28 -0500

    Update aws_eks-cluster.tf

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -2,53 +2,24 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name                   = "${var.deployment_name}-${random_string.unique_id.result}"
-  kubernetes_version     = "1.30"
-  vpc_id                 = module.vpc.vpc_id
-  subnet_ids             = module.vpc.private_subnets
+  name               = "${var.deployment_name}-${random_string.unique_id.result}"
+  kubernetes_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   endpoint_public_access = true
 
-  addons = {
-  coredns = {}
-  kube-proxy = {}
-  vpc-cni = {}
-  }
-
   eks_managed_node_groups = {
-    one = {
-      name           = "${var.deployment_name}-ng-last"
-      instance_types = ["t3a.large"]
-      ami_type       = "AL2023_x86_64_STANDARD"
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
 
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
+      ami_type = "AL2023_x86_64_STANDARD"
+
+      capacity_type = "ON_DEMAND"
     }
   }
 }
-
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 21.0"
-
-#   name               = "${var.deployment_name}-${random_string.unique_id.result}"
-#   kubernetes_version = "1.30"
-
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
-
-#   endpoint_public_access = true
-
-#   eks_managed_node_groups = {
-#     default = {
-#       instance_types = ["t3.medium"]
-#       min_size       = 1
-#       max_size       = 2
-#       desired_size   = 1
-
-#       ami_type = "AL2023_x86_64_STANDARD"
-
-#       capacity_type = "ON_DEMAND"
-#     }
-#   }
-# }
