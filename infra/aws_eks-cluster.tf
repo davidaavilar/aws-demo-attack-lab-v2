08d984f303233b1de6916bfea009738c567b7135
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-04-02 14:54:04 -0500

    Update aws_eks-cluster.tf

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -1,23 +1,23 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 21.0"
 
-  name                   = "${var.deployment_name}-${random_string.unique_id.result}"
-  kubernetes_version     = "1.30"
-  vpc_id                 = module.vpc.vpc_id
-  subnet_ids             = module.vpc.private_subnets
-  endpoint_public_access = true
+#   name                   = "${var.deployment_name}-${random_string.unique_id.result}"
+#   kubernetes_version     = "1.31"
+#   vpc_id                 = module.vpc.vpc_id
+#   subnet_ids             = module.vpc.private_subnets
+#   endpoint_public_access = true
 
-  eks_managed_node_groups = {
-    one = {
-      name           = "${var.deployment_name}-ng1"
-      instance_types = ["t3a.large"]
+#   eks_managed_node_groups = {
+#     one = {
+#       name           = "${var.deployment_name}-ng1"
+#       instance_types = ["t3.xlarge"]
 
-      ami_type = "AL2023_x86_64_STANDARD"
+#       ami_type = "AL2023_x86_64_STANDARD"
 
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
-    }
-  }
-}
+#       min_size     = 2
+#       max_size     = 3
+#       desired_size = 2
+#     }
+#   }
+# }
