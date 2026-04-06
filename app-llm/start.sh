c548b235d45305cc57ccec9939db3689f5ac9dad
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-04-06 05:58:59 -0500

    Update start.sh

--- app-llm/start.sh
+++ app-llm/start.sh
@@ -4,10 +4,12 @@ set -e
 ollama serve &
 PID=$!
 
+sleep 5
+
 # wait until ollama respond
-until curl -s http://localhost:11434 > /dev/null; do
-  sleep 1
-done
+# until curl -s http://localhost:11434 > /dev/null; do
+#   sleep 1
+# done
 
 ollama pull qwen2.5:7b
 
