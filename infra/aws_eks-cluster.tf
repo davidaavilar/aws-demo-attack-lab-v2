116a2a8747d640297280e69bb1bd62c166e95fab
David Avila <33632789+davidaavilar@users.noreply.github.com>
2026-04-29 17:57:49 -0500

    new deploy

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -1,64 +1,64 @@
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-output "current_principal_arn" {
-  value = data.aws_caller_identity.current.arn
-}
+# output "current_principal_arn" {
+#   value = data.aws_caller_identity.current.arn
+# }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 21.0"
 
-  name               = "${var.deployment_name}-${random_string.unique_id.result}"
-  kubernetes_version = "1.30"
+#   name               = "${var.deployment_name}-${random_string.unique_id.result}"
+#   kubernetes_version = "1.30"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
 
-  endpoint_public_access = true
-  enable_cluster_creator_admin_permissions = true
+#   endpoint_public_access = true
+#   enable_cluster_creator_admin_permissions = true
 
-  addons = {
-    vpc-cni = {
-      most_recent = true
-      before_compute = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    coredns = {
-      most_recent = true
-    }
-  }
+#   addons = {
+#     vpc-cni = {
+#       most_recent = true
+#       before_compute = true
+#     }
+#     kube-proxy = {
+#       most_recent = true
+#     }
+#     coredns = {
+#       most_recent = true
+#     }
+#   }
 
-  eks_managed_node_groups = {
-    default_v3 = {
-      instance_types = ["t3a.large"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+#   eks_managed_node_groups = {
+#     default_v3 = {
+#       instance_types = ["t3a.large"]
+#       min_size       = 1
+#       max_size       = 2
+#       desired_size   = 1
 
-      ami_type = "AL2023_x86_64_STANDARD"
+#       ami_type = "AL2023_x86_64_STANDARD"
 
-      capacity_type = "ON_DEMAND"
+#       capacity_type = "ON_DEMAND"
 
-      use_custom_launch_template = false
-      disk_size = 100
-    }
-    # llm = {
-    #   instance_types = ["g5.xlarge"]
-    #   min_size       = 1
-    #   max_size       = 2
-    #   desired_size   = 1
+#       use_custom_launch_template = false
+#       disk_size = 100
+#     }
+#     # llm = {
+#     #   instance_types = ["g5.xlarge"]
+#     #   min_size       = 1
+#     #   max_size       = 2
+#     #   desired_size   = 1
 
-    #   ami_type       = "AL2023_x86_64_NVIDIA"
-    #   capacity_type  = "ON_DEMAND"
+#     #   ami_type       = "AL2023_x86_64_NVIDIA"
+#     #   capacity_type  = "ON_DEMAND"
 
-    #   use_custom_launch_template = false
-    #   disk_size = 100
+#     #   use_custom_launch_template = false
+#     #   disk_size = 100
 
-    #   labels = {
-    #     workload = "llm"
-    #   }
-    # }
-  }
-}
+#     #   labels = {
+#     #     workload = "llm"
+#     #   }
+#     # }
+#   }
+# }
